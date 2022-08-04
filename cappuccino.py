500555c53da157efc3d809bfb378cd66c82c6860
Robatortas <robatortas1@gmail.com>
2022-08-03 23:16:16 -0700

    Cappuccino kinda works now

--- cappuccino.py
+++ cappuccino.py
@@ -1,20 +1,21 @@
 import os
 from cryptography.fernet import Fernet
 
-# Cappuccino is for the file decryption.
-
-files = []
-
 def getChocoContents():
     with open("choco.mocha", "rb") as choco:
         key = choco.read()
         return key
-
+        
+def decrypt(file):
+    with open(file, "rb") as choco:
+        loneKey = choco.read()
+        decrypted = Fernet(getChocoContents()).decrypt(loneKey)
+    with open(file, "wb") as final:
+        final.write(decrypted)
+        
 for file in os.listdir():
-    if file == "mocha.py":
+    if file == "mocha.py" or file == "cappuccino.py" or file == "choco.mocha":
         continue
-     
-    #print(getChocoContents())
-    
-def decryptFiles():
-    
+    if os.path.isFile(file):
+        decrypt(file)
+        
